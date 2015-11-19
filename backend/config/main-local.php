60f27ddd581cd6bf0b16b75724840acb3e5d64e3
Nguyen Tuan Sieu <tuan@sieulog.com>
2015-11-19 02:12:37 +0000

    UPdate gitignore

--- backend/config/main-local.php
+++ backend/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 's3SP2J68egqOUOvCk5MKwVF8nPezBS86',
+            'cookieValidationKey' => 'tpMfL8c6-I2JkZL753gJcNkwkLUM1xLY',
         ],
     ],
 ];
--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -3,7 +3,7 @@ return [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'cvxJbJi9sFeoci-aZ7_wBcSk3B9IUgxw',
+            'cookieValidationKey' => 'hUqDYrJhdCFc6Qr6ppmXgSs8vTpKTLmc',
         ],
     ],
 ];
